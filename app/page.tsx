c2a248dbd377e1dd311bc0d5671424b0eeef4ed4
YeeeTTT8 <rishitpradeep90@gmail.com>
2025-03-26 14:58:24 +0530

    Next JS Mastery

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <h1 className="text-3xl text-violet-700 font-black" >Welcome to the world of Next JS</h1>
+  );
+}
+
+export default Home;
